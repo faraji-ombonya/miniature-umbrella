617752fdb9ff0ee2e5b8593a8b43341d5e787cc2
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-23 14:27:08 +0300

    feat: setup notification routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	"github.com/faraji-fuji/miniature-umbrella/src/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	router := gin.Default()
+
+	notifications := router.Group("/notifications")
+	{
+		notifications.GET("/", controllers.GetNotifications)
+		notifications.GET("/:id", controllers.GetNotification)
+		notifications.POST("/", controllers.CreateNotification)
+		notifications.PUT("/:id", controllers.UpdateNotification)
+		notifications.DELETE("/:id", controllers.DeleteNotification)
+	}
+
+	router.Run(":8080")
+}
