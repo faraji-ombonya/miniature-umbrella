13ca53e85db3007c68b3d8bbf1686ba1932dfcc6
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-23 15:43:05 +0300

    feat: setup a database connection

--- src/models/models.go
+++ src/models/models.go
@@ -1,4 +1,27 @@
 package models
 
-type Notification struct {
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Sql *gorm.DB
+
+func InitDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DSN")
+
+	Sql, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	Sql.AutoMigrate(&Notification{})
 }
